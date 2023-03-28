bb3044cc86043a5c660cd3988187b5aab12ef616
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-27 22:35:11 -0600

    "Save."

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 // Create a webpage with a 16x16 grid of square divs.
 // create row containing 16 divs
-// append 15 identical rows below
+// create row
+// insert 16 divs as cells
+// create row 15 more times
 
-const container = document.getElementById("container")
 
-let row = document.createElement("div");
-row.setAttribute("id", "row")
-row.innerText = "hey";
+const container = document.getElementById("container");
 
-container.appendChild(row);
+let cell;
 
-let cell = document.createElement("div");
-cell.setAttribute("id", "cell");
-cell.innerText = "holla";
-
-row.appendChild(cell);
-
-newRow();
 
 function newRow() {
-    for (let col = 0; col <= 16; col++) {
-    cell = document.createElement("div");
-    cell.setAttribute("id", "cell");
-    cell.innerText = "holla";
-    row.appendChild(cell);
-    console.log(col);
+    let row = document.createElement("div");
+    row.setAttribute("id", "row");
+    container.appendChild(row);
+    for (let col = 0; col < 16; col++) {
+        cell = document.createElement("div");
+        cell.setAttribute("id", "cell");
+        cell.innerText = "holla";
+        row.appendChild(cell);
     }
 }
 
-function newGrid() {
 
-}
+newRow();
